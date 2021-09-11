65839c3b56c2f003f200252e27eb6c7ed533b49c
Magnusyatina <sergey37192@yandex.ru>
2021-09-11 23:36:54 +0300

    main: delete problem method. Problem with performance

--- src/main/java/filters/converters/ValueTypeConverter.java
+++ src/main/java/filters/converters/ValueTypeConverter.java
@@ -3,9 +3,6 @@ package filters.converters;
 import handlers.AbstractVisitorViewer;
 import handlers.Handler;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class ValueTypeConverter extends AbstractVisitorViewer {
 
     public ValueTypeConverter(Handler subHandler) {
@@ -13,10 +10,12 @@ public class ValueTypeConverter extends AbstractVisitorViewer {
     }
 
     @Override
-    protected Object defaultHandle(Object... params) {
-        String constraintKey = getConstraintKey(params);
-        if(!defaultHandles.contains(constraintKey))
-            defaultHandles.add(constraintKey);
-        return super.defaultHandle(params);
+    public Object handle(Object... params) {
+        try {
+            return super.handle(params);
+        } catch (Exception exception) {
+            return super.defaultHandle(params);
+        }
     }
+
 }
